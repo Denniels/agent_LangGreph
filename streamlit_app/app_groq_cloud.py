f664e71fb7c325bbe7b518c65dee3d384247a29d
Daniel Mardones <daniel.mardones@integralservicespa.cl>
2025-09-26 14:47:46 -0300

    update url jetson api

--- streamlit_app/app_groq_cloud.py
+++ streamlit_app/app_groq_cloud.py
@@ -44,7 +44,7 @@ try:
     
     # Variables de configuración
     GROQ_API_KEY = os.getenv('GROQ_API_KEY')
-    JETSON_API_URL = "https://integrate-del-peers-jefferson.trycloudflare.com"
+    JETSON_API_URL = "https://personnel-newsletters-jade-insertion.trycloudflare.com"
     
 except ImportError as e:
     st.error(f"❌ Error importando módulos: {str(e)}")
